35331ab8e0f9700d9d6159371f873062d4b83c0a
ArunTeltia <teltia.arun18@gmail.com>
2020-06-05 17:07:33 +0530

    InterInfo Form is added

--- routes/internRouter.js
+++ routes/internRouter.js
@@ -1,16 +1,69 @@
 const router = require("express").Router();
 const connection = require("../config/DBconnection");
+const {
+  check,
+  validationResult
+} = require("express-validator");
 const db = require("../config/makeDB");
 
 router.get("/form", (req, res) => {
-    res.render("InternInfo");
-})
+  res.render("InternInfo", {
+    errors: []
+  });
+});
 
+router.post(
+  "/form",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("role", "Please enter the role").not().isEmpty(),
+    check("dow", "Enter description of work").not().isEmpty(),
+    check("sdate", "Enter starting date").not().isEmpty(),
+    check("edate", "Enter ending date").not().isEmpty(),
+    check("stipend", "Enter the stipend").notEmpty().isNumeric(),
+    check("source", "Enter HiredVia").not().isEmpty(),
+    check("certi", "Enter the certificate number of 8 digits").isLength({
+      min: 8,
+      max: 8,
+    }),
+    check("institute", "Enter the name of Institute").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.render("InternInfo", {
+        errors: errors
+        // errors:[]
+      });
+    }
 
-router.post("/form", (req, res) => {
-    console.log(req.body);
-})
+    // console.log(req.body);
+    let obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    var post = {
+      Name: obj.name,
+      Email: obj.email,
+      Role: obj.role,
+      DescriptionOfWork: obj.dow,
+      StartDate: obj.sdate,
+      EndDate: obj.edate,
+      StipendPaid: obj.stipend,
+      HiredVia: obj.source,
+      CertiNum: obj.certi,
+      InstituteName: obj.institute,
+    };
 
+    var qry = "INSERT into Intern SET ?";
+    connection.query(qry, post, (err, results, fields) => {
+      if (err) throw err;
+      console.log(results);
+      // alert("Reviewers are added");
+    });
+  }
+);
 
+router.get("/data", (req, res) => {});
 
 module.exports = router;
