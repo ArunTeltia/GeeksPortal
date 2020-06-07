ab6a262c415df9d5bcec4c54c14eba6d3e46a0bf
rhythm jayee <jayeerythm8@gmail.com>
2020-06-07 23:50:56 +0530

    added pagiation to homearticles

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,40 +2,108 @@ const router = require("express").Router();
 const connection = require("../config/DBconnection");
 const db = require("../config/makeDB");
 
-router
-  .route("/")
-  .get((req, res) => {
-    var t = "All Articles";
-    var sql =
-      "select A.Id as Id, A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
-    connection.query(sql, (err, results, fields) => {
-      if (err) {
-        res.render("404");
-        return console.error(err.message);
-      }
-      ar = results;
+// router.get("/",(req, res) => {
+//     var t = "All Articles";
+//     var sql =
+//       "select A.Id as Id, A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
+//     connection.query(sql, (err, results, fields) => {
+//       if (err) {
+//         res.render("404");
+//         return console.error(err.message);
+//       }
+//       ar = results;
 
-      if (req.user) {
-        res.render("homearticle", {
-          posts: ar,
-          head: "All Articles",
-          photo: req.user.Photo,
-          user: req.user,
-        });
-      } else {
-        res.render("homearticle", {
-          posts: ar,
-          head: t,
-          photo: "#",
-          user: req.user,
-        });
-      }
-    });
-  })
-  .post((req, res) => {
+//       if (req.user) {
+//         res.render("homearticle", {
+//           posts: ar,
+//           head: "All Articles",
+//           photo: req.user.Photo,
+//           user: req.user,
+//         });
+//       } else {
+//         res.render("homearticle", {
+//           posts: ar,
+//           head: t,
+//           photo: "#",
+//           user: req.user,
+//         });
+//       }
+//     });
+//   });
+router.get("/",(req, res) => {
+  var t = "All Articles";
+  const limit = 1;
+  const page = +req.query.page || 1;
+  const offset = (page - 1) * limit;
+  let totalArticles=0;
+
+  var sql="select count(*) as count from `AllArticles`;"
+  connection.query(sql, (err, results, fields) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    totalArticles=results[0].count; 
+    // console.log(totalArticles);
+  });
+
+
+  var sql =
+    "select A.Id as Id, A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head limit "+limit+" OFFSET "+offset+";";
+  connection.query(sql, (err, results, fields) => {
+    if (err) {
+      res.render("404");
+      return console.error(err.message);
+    }
+    ar = results;
+
+    if (req.user) {
+      res.render("homearticle", {
+        posts: ar,
+        head: t,
+        photo: "#",
+        user: req.user,
+        currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
+      });
+    } else {
+      res.render("homearticle", {
+        posts: ar,
+        head: t,
+        photo: "#",
+        user: req.user,
+        currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
+      });
+    }
+  });
+});
+  router.post("/",(req, res) => {
+    const limit = 1;
+    const page = +req.query.page || 1;
+    const offset = (page - 1) * limit;
+    let totalArticles=1;
     var t = "All Articles";
+
     const obj = JSON.parse(JSON.stringify(req.body));
     console.log(obj);
+    // var sql="select count(*) as count From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Head='" +
+    // obj.search +
+    // "'GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;"
+    // connection.query(sql, (err, results, fields) => {
+    //   if (err) {
+    //     return console.error(err.message);
+    //   }
+    //   totalArticles=results[0].count; 
+    //   // console.log(totalArticles);
+    // });
     var sql =
       "select A.Id as Id,A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Head='" +
       obj.search +
@@ -53,6 +121,12 @@ router
           head: t,
           photo: req.user.Photo,
           user: req.user,
+          currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
         });
       } else {
         res.render("homearticle", {
@@ -60,49 +134,70 @@ router
           head: t,
           photo: "#",
           user: req.user,
+          currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
         });
       }
     });
   });
 
-router.get("/type/:type", (req, res) => {
-  var t = req.params.type;
-  var sql =
-    "select A.Id as Id,A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Type='" +
-    req.params.type +
-    "'GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
+// router.get("/type/:type", (req, res) => {
+//   var t = req.params.type;
+//   var sql =
+//     "select A.Id as Id,A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Type='" +
+//     req.params.type +
+//     "'GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
+//   connection.query(sql, (err, results, fields) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     ar = results;
+//     console.log(ar);
+
+//     console.log(t);
+
+//     if (req.user) {
+//       res.render("homearticle", {
+//         posts: ar,
+//         head: t,
+//         photo: req.user.Photo,
+//         user: req.user,
+//       });
+//     } else {
+//       res.render("homearticle", {
+//         posts: ar,
+//         head: t,
+//         photo: "#",
+//         user: req.user,
+//       });
+//     }
+//   });
+// });
+
+router.get("/tag/:title", (req, res) => {
+  const limit = 1;
+  const page = +req.query.page || 1;
+  const offset = (page - 1) * limit;
+  let totalArticles=0;
+
+  var sql="select count(*) as count from (select A.Id as Id, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog From Tags T Left Join  ItemTags I on T.TagId=I.TagId Left Join AllArticles A on I.ArticleId = A.Id where T.Title ='" +
+  req.params.title +
+  "' and A.Status='Accepted'  group by A.Id,A.Lang,A.Level,A.Blog,A.Head ) AA left join (select A.Id as Id, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head) as BB on AA.Id = BB.Id;";
   connection.query(sql, (err, results, fields) => {
     if (err) {
       return console.error(err.message);
     }
-    ar = results;
-    console.log(ar);
-
-    console.log(t);
-
-    if (req.user) {
-      res.render("homearticle", {
-        posts: ar,
-        head: t,
-        photo: req.user.Photo,
-        user: req.user,
-      });
-    } else {
-      res.render("homearticle", {
-        posts: ar,
-        head: t,
-        photo: "#",
-        user: req.user,
-      });
-    }
+    totalArticles=results[0].count; 
+    // console.log(totalArticles);
   });
-});
-
-router.get("/tag/:title", (req, res) => {
   var sql =
     "select * from (select A.Id as Id, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog From Tags T Left Join  ItemTags I on T.TagId=I.TagId Left Join AllArticles A on I.ArticleId = A.Id where T.Title ='" +
     req.params.title +
-    "' and A.Status='Accepted'  group by A.Id,A.Lang,A.Level,A.Blog,A.Head ) AA left join (select A.Id as Id, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head) as BB on AA.Id = BB.Id;";
+    "' and A.Status='Accepted'  group by A.Id,A.Lang,A.Level,A.Blog,A.Head ) AA left join (select A.Id as Id, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted'  GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head) as BB on AA.Id = BB.Id limit "+limit+" OFFSET "+offset+";";
   connection.query(sql, (err, results, fields) => {
     if (err) {
       return console.error(err.message);
@@ -114,6 +209,12 @@ router.get("/tag/:title", (req, res) => {
         head: "All Articles",
         photo: req.user.Photo,
         user: req.user,
+        currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
       });
     } else {
       res.render("homearticle", {
@@ -121,31 +222,37 @@ router.get("/tag/:title", (req, res) => {
         head: "All Articles",
         photo: "#",
         user: req.user,
+        currentPage:page,
+        hasNextPage:limit*page<totalArticles,
+        hasPreviousPage:page>1,
+        nextPage:page+1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalArticles/limit)
       });
     }
   });
 });
 
-router.get("/Level/:level", (req, res) => {
-  var sql =
-    "select A.Id as Id,A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Level='" +
-    req.params.level +
-    "'GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
-  connection.query(sql, (err, results, fields) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    ar = results;
-    console.log(ar);
+// router.get("/Level/:level", (req, res) => {
+//   var sql =
+//     "select A.Id as Id,A.Type as Type, A.Lang as Lang,A.Level as Level , A.Head as Head, A.Blog as Blog, GROUP_CONCAT(T.Title) as Title From AllArticles A Left Join ItemTags I on A.Id = I.ArticleId Left Join Tags T On T.TagId = I.TagId where A.Status = 'Accepted' and A.Level='" +
+//     req.params.level +
+//     "'GROUP BY A.Id,A.Lang,A.Level,A.Blog,A.Head;";
+//   connection.query(sql, (err, results, fields) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     ar = results;
+//     console.log(ar);
 
-    res.render("homearticle", {
-      posts: ar,
-      head: "All Articles",
-      photo: req.user.Photo,
-      user: req.user,
-    });
-  });
-});
+//     res.render("homearticle", {
+//       posts: ar,
+//       head: "All Articles",
+//       photo: req.user.Photo,
+//       user: req.user,
+//     });
+//   });
+// });
 
 router.get("/:head-:postId", (req, res) => {
   let moreArt = [];
