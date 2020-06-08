b5356dd2cfe61971d6c0a27f08c811944c778958
rhythm jayee <jayeerythm8@gmail.com>
2020-06-08 20:37:20 +0530

    nodemailer added

--- routes/contactUsRouter.js
+++ routes/contactUsRouter.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const nodemailer = require('nodemailer')
 
 router.route("/")
 .get((req,res)=>{
@@ -16,7 +17,35 @@ router.route("/")
       }
 })
 .post((req,res)=>{
-    
+    let obj=JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'rhythm03rhythm@gmail.com',
+          pass: 'netflix&chill'
+        }
+      })
+
+      const mailOpts = {
+        from: obj.email, // This is ignored by Gmail
+        to: 'rhythm03rhythm@gmail.com',
+        subject: 'New message from contact form at geekspotal',
+        text: `${obj.name} (${obj.email}) says: ${obj.message}`
+      }
+
+      smtpTrans.sendMail(mailOpts, (error, response) => {
+        if (error) {
+          console.log(error);
+           // Show a page indicating failure
+        }
+        else {
+        //   res.render('contact-success') // Show a page indicating success
+        res.redirect("/contactus");
+        }
+      })
 })
 
 module.exports=router;
