0867ed6d186e1c47f45d57742442f6521979d1db
rhythm jayee <jayeerythm8@gmail.com>
2020-06-15 15:59:55 +0530

    fb api key

--- config/keys.js
+++ config/keys.js
@@ -8,8 +8,8 @@ module.exports = {
     cookieKey: "Our secret key",
   },
   facebook: {
-    appID: "270233710769067",
-    appSecret: "5a8016a9f1aa07af51e7caa8b560debf",
+    appID: "805473220258170",
+    appSecret: "914e7e89a48967158d2d490e94f3a680",
   },
   sendgridKey:"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"
 };
