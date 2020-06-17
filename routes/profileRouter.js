7581d4f40ad6be83dd9cade693b94d16e5693c1a
ArunTeltia <teltia.arun18@gmail.com>
2020-06-18 03:24:46 +0530

    error handling at profile

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -10,180 +10,189 @@ const connection = require("../config/DBconnection");
 profile
   .route("/")
   .get(authCheck, (req, res) => {
-    async function Render() {
-      try {
-        const r = await db.query(
-          "select * from Users where ID='" + req.user.ID + "'"
-        );
-        return r;
-      } catch (err) {
-        console.log(err);
-      }
-      //   finally {
-      //       await db.close();
-      //     }
-    }
-    Render().then((results) => {
-      res.render("profile", {
-        details: results[0],
-      });
-    });
-  })
-  .post(authCheck, (req, res) => {
-    console.log(req.body);
-    console.log(req.files);
-
-    let usr = req.body.usr.trim();
-    if (req.body.usr === null || !usr.match(/^[0-9a-z]+$/)) {
-      req.session.message = {
-        type: "danger",
-        intro: "Invalid UserName",
-        message: "please enter valid username",
-      };
-      res.redirect("/profile");
-    } else {
-      if (req.files) {
-        var file = req.files.uploaded_image;
-        // console.log(file);
-        var img_name = file.name;
-
-        if (
-          file.mimetype == "image/jpeg" ||
-          file.mimetype == "image/png" ||
-          file.mimetype == "image/gif"
-        ) {
-          file.mv("public/ProfileImages/" + file.name, function (err) {
-            let image = "/ProfileImages/" + file.name;
-            if (err) return res.status(500).send(err);
-
-            var sql =
-              "UPDATE `Users` set Photo='" +
-              image +
-              "'  where ID=" +
-              req.user.ID;
-
-            var query = connection.query(sql, function (err, result) {
-              if (err) {
-                console.log(err);
-              }
-              console.log(result);
-            });
-          });
-        } else {
-          req.session.message = {
-            type: "danger",
-            intro: "Invalid Image format",
-            message: "please upload file with '.png','.gif','.jpg'",
-          };
-          res.redirect("/profile");
-        }
-      }
-
-      async function update() {
+    try {
+      async function Render() {
         try {
-          let f = 0;
-          var c1 = await db.query(
-            "select ID from Users where UserName='" + req.body.usr.trim() + "'"
+          const r = await db.query(
+            "select * from Users where ID='" + req.user.ID + "'"
           );
-          if (c1.length === 0 || c1[0].ID === req.user.ID) {
-            f = 1;
-            var c2 = await db.query(
-              "UPDATE  `Users` set UserName='" +
-                usr +
-                "' where ID='" + req.user.ID + "'"
-            );
-
-            if (req.body.email !== "") {
-              var c3 = await db.query(
-                "UPDATE  `Users` set Email='" +
-                  req.body.email +
-                  "' where ID='" + req.user.ID + "'"
-              );
-            }
-            if (req.body.Name !== "") {
-              var c4 = await db.query(
-                "UPDATE  `Users` set DisplayName='" +
-                  req.body.Name +
-                  "' where ID='" + req.user.ID + "'"
-              );
-            }
-            if (req.body.ins !== "") {
-              var c5 = await db.query(
-                "UPDATE  `Users` set Institute='" +
-                  req.body.ins +
-                  "' where ID='" + req.user.ID + "'"
-              );
-            }
-            if (req.body.cntry !== "") {
-              var c7 = await db.query(
-                "UPDATE  `Users` set Country='" +
-                  req.body.cntry +
-                  "' where ID='" + req.user.ID + "'"
-              );
-            }
-            if (req.body.dob !== "") {
-              var c6 = await db.query(
-                "UPDATE  `Users` set DOB='" +
-                  req.body.dob +
-                  "' where ID='" + req.user.ID + "'"
-              );
-            }
-            if (req.body.phone !== "") {
-              if (req.body.phone.length === 10)
-                var c7 = await db.query(
-                  "UPDATE  `Users` set PhoneNo='" +
-                    req.body.phone +
-                    "' where ID='" + req.user.ID + "'"
-                );
-              else if (req.body.phone.length !== 10) {
-                f = 2;
-              }
-            }
-          }
-          return f;
+          return r;
         } catch (err) {
           console.log(err);
         }
-        // finally {
-        //     await db.close();
-        //   }
+        //   finally {
+        //       await db.close();
+        //     }
       }
-      update().then((result) => {
-        if (result === 0) {
-          req.session.message = {
-            type: "danger",
-            intro: " UserName already exist",
-            message: "please enter different username",
-          };
-          res.redirect("/profile");
-        } else if (result === 1) {
-          req.session.message = {
-            type: "success",
-            intro: " Well Done!",
-            message: "Profile updated",
-          };
-          res.redirect("/profile");
-        } else if (result === 2) {
-          req.session.message = {
-            type: "danger",
-            intro: " Invalid Phone Number",
-            message: "Please enter Valid Phone Number",
-          };
-          res.redirect("/profile");
-        }
+      Render().then((results) => {
+        res.render("profile", {
+          details: results[0],
+        });
       });
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .post(authCheck, (req, res) => {
+    try {
+      console.log(req.body);
+      console.log(req.files);
+
+      let usr = req.body.usr.trim();
+      if (req.body.usr === null || !usr.match(/^[0-9a-z]+$/)) {
+        req.session.message = {
+          type: "danger",
+          intro: "Invalid UserName",
+          message: "please enter valid username",
+        };
+        res.redirect("/profile");
+      } else {
+        if (req.files) {
+          var file = req.files.uploaded_image;
+          // console.log(file);
+          var img_name = file.name;
+
+          if (
+            file.mimetype == "image/jpeg" ||
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/gif"
+          ) {
+            file.mv("public/ProfileImages/" + file.name, function (err) {
+              let image = "/ProfileImages/" + file.name;
+              if (err) return res.status(500).send(err);
+
+              var sql =
+                "UPDATE `Users` set Photo='" +
+                image +
+                "'  where ID=" +
+                req.user.ID;
+
+              var query = connection.query(sql, function (err, result) {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(result);
+              });
+            });
+          } else {
+            req.session.message = {
+              type: "danger",
+              intro: "Invalid Image format",
+              message: "please upload file with '.png','.gif','.jpg'",
+            };
+            res.redirect("/profile");
+          }
+        }
+
+        async function update() {
+          try {
+            let f = 0;
+            var c1 = await db.query(
+              "select ID from Users where UserName='" + req.body.usr.trim() + "'"
+            );
+            if (c1.length === 0 || c1[0].ID === req.user.ID) {
+              f = 1;
+              var c2 = await db.query(
+                "UPDATE  `Users` set UserName='" +
+                usr +
+                "' where ID='" + req.user.ID + "'"
+              );
+
+              if (req.body.email !== "") {
+                var c3 = await db.query(
+                  "UPDATE  `Users` set Email='" +
+                  req.body.email +
+                  "' where ID='" + req.user.ID + "'"
+                );
+              }
+              if (req.body.Name !== "") {
+                var c4 = await db.query(
+                  "UPDATE  `Users` set DisplayName='" +
+                  req.body.Name +
+                  "' where ID='" + req.user.ID + "'"
+                );
+              }
+              if (req.body.ins !== "") {
+                var c5 = await db.query(
+                  "UPDATE  `Users` set Institute='" +
+                  req.body.ins +
+                  "' where ID='" + req.user.ID + "'"
+                );
+              }
+              if (req.body.cntry !== "") {
+                var c7 = await db.query(
+                  "UPDATE  `Users` set Country='" +
+                  req.body.cntry +
+                  "' where ID='" + req.user.ID + "'"
+                );
+              }
+              if (req.body.dob !== "") {
+                var c6 = await db.query(
+                  "UPDATE  `Users` set DOB='" +
+                  req.body.dob +
+                  "' where ID='" + req.user.ID + "'"
+                );
+              }
+              if (req.body.phone !== "") {
+                if (req.body.phone.length === 10)
+                  var c7 = await db.query(
+                    "UPDATE  `Users` set PhoneNo='" +
+                    req.body.phone +
+                    "' where ID='" + req.user.ID + "'"
+                  );
+                else if (req.body.phone.length !== 10) {
+                  f = 2;
+                }
+              }
+            }
+            return f;
+          } catch (err) {
+            console.log(err);
+          }
+          // finally {
+          //     await db.close();
+          //   }
+        }
+        update().then((result) => {
+          if (result === 0) {
+            req.session.message = {
+              type: "danger",
+              intro: " UserName already exist",
+              message: "please enter different username",
+            };
+            res.redirect("/profile");
+          } else if (result === 1) {
+            req.session.message = {
+              type: "success",
+              intro: " Well Done!",
+              message: "Profile updated",
+            };
+            res.redirect("/profile");
+          } else if (result === 2) {
+            req.session.message = {
+              type: "danger",
+              intro: " Invalid Phone Number",
+              message: "Please enter Valid Phone Number",
+            };
+            res.redirect("/profile");
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
   });
 
 
-  profile.get("/Status",authCheck,(req,res)=>{
-    let usr=[];
+profile.get("/Status", authCheck, (req, res) => {
+  try {
+    let usr = [];
     async function Render() {
       try {
         const r = await db.query(
           "select * from Users where ID='" + req.user.ID + "'"
         );
-        usr=r;
+        usr = r;
         const r1 = await db.query(
           "select * from AllArticles where UserId='" + req.user.ID + "'"
         );
@@ -191,17 +200,20 @@ profile
       } catch (err) {
         console.log(err);
       }
-      
+
     }
     Render().then((results) => {
       console.log(results)
       console.log(usr);
       res.render("ContributionStatus", {
         details: usr[0],
-        posts:results
+        posts: results
       });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = profile;
 
