0e258dcdc89ac28c3365be61bbff9c7556661011
ArunTeltia <teltia.arun18@gmail.com>
2020-06-18 03:27:10 +0530

    error handling at config/aauth

--- config/AauthCheck.js
+++ config/AauthCheck.js
@@ -1,27 +1,31 @@
 const connection = require("./DBconnection");
 
 const loggedIn = (req, res, next) => {
-  let sql = "select ID from Admin";
-  connection.query(sql, (err, results, fields) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    // console.log(results);
-    // console.log(req.user);
-    if (req.user) {
-      var found = results.find((r) => {
-        return r.ID === req.user.ID;
-      });
-      // console.log(found);
-      if (found) {
-        next();
+  try {
+    let sql = "select ID from Admin";
+    connection.query(sql, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      // console.log(results);
+      // console.log(req.user);
+      if (req.user) {
+        var found = results.find((r) => {
+          return r.ID === req.user.ID;
+        });
+        // console.log(found);
+        if (found) {
+          next();
+        } else {
+          res.redirect("/auth/admin/login");
+        }
       } else {
         res.redirect("/auth/admin/login");
       }
-    } else {
-      res.redirect("/auth/admin/login");
-    }
-  });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = loggedIn;
