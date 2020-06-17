029bb58e76bece923437ebcc12308c156c8cacb0
ArunTeltia <teltia.arun18@gmail.com>
2020-06-18 03:28:32 +0530

    error handling at config/adminpassport

--- config/Admin-passport.js
+++ config/Admin-passport.js
@@ -7,52 +7,63 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((ID, done) => {
-    var sql = "select * from Admin where ID='" + ID + "'";
-    connection.query(sql, (err, results, fields) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        // console.log(results);
-        done(null, results[0]);
-    });
+    try {
+        var sql = "select * from Admin where ID='" + ID + "'";
+        connection.query(sql, (err, results, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            // console.log(results);
+            done(null, results[0]);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 passport.use("admin-local",
     new LocalStrategy({
-            usernameField: "username",
-            passwordField: "password",
-            passReqToCallback: true,
-        },
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true,
+    },
         function (req, username, password, done) {
-            connection.query(
-                "SELECT * FROM `Admin` WHERE UserName = '" + username + "'",
-                function (err, rows) {
-                    if (err) return done(err);
-                    if (!rows.length) {
-                        return done(
-                            null,
-                            false,
-                            (req.session.message = {
-                                type: "danger",
-                                intro: "User doesnt exist",
-                                message: "please enter valid email",
-                            })
-                        );
-                    } else if (!(rows[0].Password == password)) {
-                        return done(
-                            null,
-                            false,
-                            (req.session.message = {
-                                type: "danger",
-                                intro: "Invalid Password",
-                                message: "please enter correct Password",
-                            })
-                        );
-                    }
+            try {
+                connection.query(
+                    "SELECT * FROM `Admin` WHERE UserName = '" + username + "'",
+                    function (err, rows) {
+                        if (err) return done(err);
+                        if (!rows.length) {
+                            return done(
+                                null,
+                                false,
+                                (req.session.message = {
+                                    type: "danger",
+                                    intro: "User doesnt exist",
+                                    message: "please enter valid email",
+                                })
+                            );
+                        } else if (!(rows[0].Password == password)) {
+                            return done(
+                                null,
+                                false,
+                                (req.session.message = {
+                                    type: "danger",
+                                    intro: "Invalid Password",
+                                    message: "please enter correct Password",
+                                })
+                            );
+                        }
 
-                    return done(null, rows[0]);
-                }
-            );
+                        return done(null, rows[0]);
+                    }
+                );
+            } catch (err) {
+                console.log(err);
+            }
         }
+
+
     )
+
 );
