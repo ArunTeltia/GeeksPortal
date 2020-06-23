85a78529e424f001df77e1d268432aff26a1a524
ArunTeltia <teltia.arun18@gmail.com>
2020-06-24 00:01:30 +0530

    making it ready for ses

--- server.js
+++ server.js
@@ -221,8 +221,8 @@ app.use("/", (req, res) => {
   res.render("404");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 });
 
 
